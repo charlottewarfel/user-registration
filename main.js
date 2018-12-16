b8c4db724748090812c95f1bb0c9d18ba5ccec55
Charlotte Warfel <charlottewarfel@gmail.com>
2018-12-15 17:08:23 -0800

    user-info printing below form

---
+++ main.js
@@ -0,0 +1,13 @@
+function check()
+        {
+            document.getElementById('f1').innerHTML = document.userform.firstname.value;
+            document.getElementById('f2').innerHTML = document.userform.lastname.value;
+            document.getElementById('f3').innerHTML = document.userform.address.value;
+            document.getElementById('f4').innerHTML = document.userform.city.value;
+            document.getElementById('f5').innerHTML = document.userform.state.value;
+            document.getElementById('f6').innerHTML = document.userform.zipcode.value;
+            document.getElementById('f7').innerHTML = document.userform.phone.value;
+            document.getElementById('f8').innerHTML = document.userform.email.value;
+
+            
+        }
