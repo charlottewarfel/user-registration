46a5bbad79e5366f3f24025b3f40f16fab4e67b7
Charlotte Warfel <charlottewarfel@gmail.com>
2018-12-15 17:21:03 -0800

    password and confirm password working

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ function check()
 
             
         }
+
+        var verifyPswd = function() {
+            if (document.getElementById('password').value ==
+                    document.getElementById('confirm_password').value) {
+                    document.getElementById('message').style.color = 'green';
+                    document.getElementById('message').innerHTML = 'MATCHING';
+        } else {
+                    document.getElementById('message').style.color = 'red';
+                    document.getElementById('message').innerHTML = 'NOT MATCHING';
+  }}    
